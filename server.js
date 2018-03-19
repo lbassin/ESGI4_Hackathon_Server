05cd3e6d24d7c2b11f77ec945c79ec59ff498dc7
Bassin Laurent <laurent@bassin.info>
2018-03-19 22:02:52 +0100

    Add MongoDb

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const express = require('express');
 const cors = require('cors');
 const app = express();
+
+let db = null;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.listen(3000, function () {
-    console.log('listening on 3000')
-});
 
 app.post('/api/', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
@@ -22,9 +21,25 @@ app.post('/api/', function (req, res) {
     console.log(userId);
     console.log(req.body);
 
+    db.collection('question').insertOne(req.body);
+
     const data = {
         'value': 4
     };
 
     res.send(JSON.stringify(data));
 });
+
+MongoClient.connect('mongodb://127.0.0.1:27017', (error, client) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+    db = client.db('hackathon');
+
+    app.listen(3000, function () {
+        console.log('listening on 3000')
+    });
+});
+
