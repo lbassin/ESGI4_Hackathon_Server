d99065e98d684e3271d4c235220ca688dc9ba12f
Bassin Laurent <laurent@bassin.info>
2018-03-22 23:10:55 +0100

    fix

--- features/help.js
+++ features/help.js
@@ -9,23 +9,19 @@ module.exports = () => {
                     {
                         title: 'Avoir des séries similaires',
                         desc: 'Dites : donne moi les séries similaires à {nom de votre série}',
-                        button: 'ok'
+                        button: 'Séries similaire'
                     }, {
                         title: 'Connaitre la sortie du prochain épisode d\'une série',
                         desc: 'Dites : quand sort le prochain épisode de {nom de votre série}',
-                        button: 'ok'
-                    }, {
-                        title: 'Avoir les avis d\'une série',
-                        desc: 'Dites : donne moi les avis de {nom de votre série}',
-                        button: 'ok'
+                        button: 'Prochain épisode'
                     }, {
                         title: 'Avoir le trailer d\'un film',
                         desc: 'Dites : montre moi le trailer de {nom de votre film}',
-                        button: 'ok'
+                        button: 'Trailer'
                     }, {
                         title: 'Connaitre la note d\'une série',
                         desc: 'Dites : quels sont les avis sur {nom de votre série}',
-                        button: 'ok'
+                        button: 'Note'
                     }
                 ]
             }
