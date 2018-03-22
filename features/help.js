020d088a91ae5a99e69896fe7657eb0ca6b4e1e4
xRaz <maxime.marquet1@gmail.com>
2018-03-22 22:55:09 +0100

    help

--- features/help.js
+++ features/help.js
@@ -3,16 +3,28 @@ module.exports = () => {
         let output = {
             type: 'help',
             data: {
-                message: '',
-                vocal: '',
+                message: 'Voici l\'aide disponible',
+                vocal: 'Voici comment je peux vous aider',
                 helps: [
                     {
-                        title: 'test',
-                        desc: 'desc',
+                        title: 'Avoir des séries similaires',
+                        desc: 'Dites : donne moi les séries similaires à {nom de votre série}',
                         button: 'ok'
                     }, {
-                        title: 'test',
-                        desc: 'desc',
+                        title: 'Connaitre la sortie du prochain épisode d\'une série',
+                        desc: 'Dites : quand sort le prochain épisode de {nom de votre série}',
+                        button: 'ok'
+                    }, {
+                        title: 'Avoir les avis d\'une série',
+                        desc: 'Dites : donne moi les avis de {nom de votre série}',
+                        button: 'ok'
+                    }, {
+                        title: 'Avoir le trailer d\'un film',
+                        desc: 'Dites : montre moi le trailer de {nom de votre film}',
+                        button: 'ok'
+                    }, {
+                        title: 'Connaitre la note d\'une série',
+                        desc: 'Dites : quels sont les avis sur {nom de votre série}',
                         button: 'ok'
                     }
                 ]
